bd937e96f5b1d6546a846baadfd95c11b248d02c
ent <entgithub@gmail.com>
2023-06-20 14:52:03 -0400

    added lib

--- .github/workflows/testPython.py
+++ .github/workflows/testPython.py
@@ -9,8 +9,14 @@ print(f"{os.environ['GITHUB_TOKEN']}")
 #print(f"{os.environ['REPOSITORY']}")
 
 headers = {
-    'Authorization': 'Bearer '+os.environ['GITHUB_TOKEN'] 
+    'Authorization': f"Bearer {os.environ['GITHUB_TOKEN']}"
 }
 
 login = requests.get("https://api.github.com/user", headers=headers)
 print(login.json())
+
+alerts = requests.get(
+    f"https://api.github.com/orgs/{os.environ['ORGANIZATION']}+code-scanning/alerts",
+    headers=headers
+)
+print(alerts.json())
