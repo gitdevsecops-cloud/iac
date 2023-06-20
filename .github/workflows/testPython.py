9bdefdcc6596eecb296a6b7d486afa54f8b64dfe
ent <entgithub@gmail.com>
2023-06-20 10:52:38 -0400

    added python flow

--- .github/workflows/testPython.py
+++ .github/workflows/testPython.py
@@ -1,1 +1,8 @@
+import os
 print("hello from python script")
+
+print(f"{os.environ['GITHUB_TOKEN']}")
+print(f"{os.environ['GH_TOKEN']}")
+print(f"{os.environ['ORGANIZATION']}")
+print(f"{os.environ['REPOSITORY']}")
+
