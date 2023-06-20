8c71789b8eaf6f7adf088b303b6162c2b6c87012
ent <entgithub@gmail.com>
2023-06-20 11:49:51 -0400

    added lib

--- .github/workflows/testPython.py
+++ .github/workflows/testPython.py
@@ -1,4 +1,4 @@
-from github import Auth
+from github import AccessToken
 import github
 import os
 print("hello from python script")
@@ -8,6 +8,22 @@ print(f"{os.environ['GH_TOKEN']}")
 print(f"{os.environ['ORGANIZATION']}")
 print(f"{os.environ['REPOSITORY']}")
 
-auth = Auth.Token(os.environ['GITHUB_TOKEN'])
-g = github(auth= auth)
-print(g.get_user().login)
+from github import Github
+
+# Authentication is defined via github.Auth
+from github import Auth
+
+# using an access token
+auth = Auth.Token("access_token")
+
+# First create a Github instance:
+
+# Public Web Github
+g = Github(auth=auth)
+
+# Github Enterprise with custom hostname
+g = Github(base_url="https://{hostname}/api/v3", auth=auth)
+
+# Then play with your Github objects:
+for repo in g.get_user().get_repos():
+    print(repo.name)
