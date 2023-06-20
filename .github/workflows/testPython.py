39832afeef228f4b445363e0b7869338796f4f06
ent <entgithub@gmail.com>
2023-06-20 14:41:54 -0400

    added lib

--- .github/workflows/testPython.py
+++ .github/workflows/testPython.py
@@ -1,29 +1,20 @@
-from github import AccessToken
-import github
+
 import os
+import requests
 print("hello from python script")
 
 print(f"{os.environ['GITHUB_TOKEN']}")
-print(f"{os.environ['GH_TOKEN']}")
-print(f"{os.environ['ORGANIZATION']}")
-print(f"{os.environ['REPOSITORY']}")
+#print(f"{os.environ['GH_TOKEN']}")
+#print(f"{os.environ['ORGANIZATION']}")
+#print(f"{os.environ['REPOSITORY']}")
 
-from github import Github
+from ghapi.all import GhApi
 
-# Authentication is defined via github.Auth
-from github import Auth
+api = GhApi(token=os.environ['GITHUB_TOKEN'])
 
-# using an access token
-auth = Auth.Token("access_token")
+headers = {
+    'Authorization': 'Bearer '+os.environ['GITHUB_TOKEN'] 
+}
 
-# First create a Github instance:
-
-# Public Web Github
-g = Github(auth=auth)
-
-# Github Enterprise with custom hostname
-g = Github(base_url="https://{hostname}/api/v3", auth=auth)
-
-# Then play with your Github objects:
-for repo in g.get_user().get_repos():
-    print(repo.name)
+login = requests.get("https://api.github.com/user", headers=headers)
+print(login.json())
