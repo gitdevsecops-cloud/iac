e9f5ee9ea27884af053f51fa70619d0348118093
ent <entgithub@gmail.com>
2023-06-20 14:43:12 -0400

    added lib

--- .github/workflows/testPython.py
+++ .github/workflows/testPython.py
@@ -8,10 +8,6 @@ print(f"{os.environ['GITHUB_TOKEN']}")
 #print(f"{os.environ['ORGANIZATION']}")
 #print(f"{os.environ['REPOSITORY']}")
 
-from ghapi.all import GhApi
-
-api = GhApi(token=os.environ['GITHUB_TOKEN'])
-
 headers = {
     'Authorization': 'Bearer '+os.environ['GITHUB_TOKEN'] 
 }
